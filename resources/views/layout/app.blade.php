f0b0ecbf94490abba178f48f9849f8fdcf2f8dc1
semihycl2397 <smhycl2341@gmail.com>
2021-08-17 17:24:43 +0300

    bug fixed

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -32,23 +32,9 @@
             <ul class="dropdown-menu">
                 <li><a href="#"><i class="icon-user"></i> My Profile</a></li>
                 <li class="divider"></li>
-                <li><a href="#"><i class="icon-check"></i> My Tasks</a></li>
-                <li class="divider"></li>
                 <li><a href="login"><i class="icon-key"></i> Log Out</a></li>
             </ul>
         </li>
-        <li class="dropdown" id="menu-messages"><a href="#" data-toggle="dropdown" data-target="#menu-messages"
-                                                   class="dropdown-toggle"><i class="icon icon-envelope"></i> <span
-                    class="text">Messages</span><b class="caret"></b></a>
-            <ul class="dropdown-menu">
-                <li><a class="sAdd" title="" href="#"><i class="icon-plus"></i> new message</a></li>
-                <li class="divider"></li>
-                <li><a class="sInbox" title="" href="#"><i class="icon-envelope"></i> inbox</a></li>
-                <li class="divider"></li>
-                <li><a class="sOutbox" title="" href="#"><i class="icon-arrow-up"></i> outbox</a></li>
-                <li class="divider"></li>
-                <li><a class="sTrash" title="" href="#"><i class="icon-trash"></i> trash</a></li>
-            </ul>
         </li>
         <li class=""><a href="{{route('siteconfig.index')}}"><i class="icon icon-pencil"></i>
                 <span> Settings </span></a></li>
