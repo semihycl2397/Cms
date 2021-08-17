9ed565fed71ae149673c14c5fa247f504aafaac3
semihycl2397 <smhycl2341@gmail.com>
2021-08-17 16:01:13 +0300

    bug fixed

--- resources/views/panel/Communication/index.blade.php
+++ resources/views/panel/Communication/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layout.app')
 @section('content')
 
+
+
+
+
+
+
+
     <!--Footer-part-->
     <div class="row-fluid">
         <div id="footer" class="span12"> 2021 &copy; Semih Yücel. </a> </div>
