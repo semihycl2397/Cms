f147ea2c8396796a2ba01a2f9e8e6d60f4612821
semihycl2397 <smhycl2341@gmail.com>
2021-08-17 17:15:58 +0300

    bug fixed

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -118,7 +118,7 @@
 <div id="content">
     <!--breadcrumbs-->
     <div id="content-header">
-        <div id="breadcrumb"><a href="/" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a></div>
+        <div id="breadcrumb"><a href="/admin" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a></div>
 
     </div>
     <!--End-breadcrumbs-->
