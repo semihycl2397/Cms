d868b4f5ceae2f0b29c3314fbc87de173fbc1828
semihycl2397 <smhycl2341@gmail.com>
2021-08-18 15:06:53 +0300

    view has been edited

--- resources/views/panel/SiteConfig/index.blade.php
+++ resources/views/panel/SiteConfig/index.blade.php
@@ -1,7 +1,59 @@
 @extends('layout.app')
 @section('content')
+<body>
 
-    <!--Footer-part-->
+<div class="container">
+    <div class="container">
+        <form>
+            <div class="form-group">
+                <label for="comment">Comment:</label>
+                <textarea class="form-control" rows="3" id=""></textarea>
+
+        <div class="form-group">
+            <label for="usr">Adres:</label>
+            <input type="text" class="form-control" rows="3" id="">
+        </div>
+        <div class="form-group">
+            <label for="usr">Telefon:</label>
+            <input type="text" class="form-control" rows="3" id="">
+        </div>
+        <div class="form-group">
+            <label for="usr">Fax:</label>
+            <input type="text" class="form-control" rows="3" id="">
+        </div>
+        <div class="form-group">
+            <label for="usr">E-mail:</label>
+            <input type="text" class="form-control" rows="3" id="">
+        </div>
+        <div class="form-group">
+            <label for="usr">Facebook:</label>
+            <input type="text" class="form-control" rows="3" id="">
+        </div>
+        <div class="form-group">
+            <label for="usr">Twitter:</label>
+            <input type="text" class="form-control" rows="3" id="">
+        </div>
+        <div class="form-group">
+            <label for="usr">Instagram:</label>
+            <input type="text" class="form-control" rows="3" id="">
+        </div>
+        <div class="form-group">
+            <label for="usr">Youtube:</label>
+            <input type="text" class="form-control" rows="3" id="">
+        </div>
+        <div class="form-group">
+            <label for="usr">Blog:</label>
+            <input type="text" class="form-control" rows="3" id="">
+        </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
+
+<!--Footer-part-->
     <div class="row-fluid">
         <div id="footer" class="span12"> 2021 &copy; Semih Yücel. </a> </div>
     </div>
