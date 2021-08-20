3087560f31681f692e4c01d9c175051c73b04158
semihycl2397 <smhycl2341@gmail.com>
2021-08-20 16:41:37 +0300

    view has been edited

--- resources/views/panel/SiteConfig/index.blade.php
+++ resources/views/panel/SiteConfig/index.blade.php
@@ -5,43 +5,43 @@
 
         <form>
             <div class="form-group">
-                <label for="exampleFormControlTextarea1"> Bilgi Alanı:</label>
+                <label for="exampleFormControlTextarea1"><b> Bilgi Alanı:</b></label>
                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Adres:</label>
+                <label for="exampleInputEmail1"><b>Adres:</b></label>
                 <input type="text" class="form-control">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Telefon:</label>
+                <label for="exampleInputEmail1"><b>Telefon:</b></label>
                 <input type="text" class="form-control">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Fax:</label>
+                <label for="exampleInputEmail1"><b>Fax:</b></label>
                 <input type="text" class="form-control">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">E-mail:</label>
+                <label for="exampleInputEmail1"><b>E-mail:</b></label>
                 <input type="email" class="form-control">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Facebook:</label>
+                <label for="exampleInputEmail1"><b>Facebook:</b></label>
                 <input type="text" class="form-control">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Twitter:</label>
+                <label for="exampleInputEmail1"><b>Twitter:</b></label>
                 <input type="text" class="form-control">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Instagram:</label>
+                <label for="exampleInputEmail1"><b>Instagram:</b></label>
                 <input type="text" class="form-control">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Youtube:</label>
+                <label for="exampleInputEmail1"><b>Youtube:</b></label>
                 <input type="text" class="form-control">
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Blog:</label>
+                <label for="exampleInputEmail1"><b>Blog:</b></label>
                 <input type="text" class="form-control">
             </div>
             <button type="submit" class="btn btn-primary">Değişiklikleri Kaydet</button>
