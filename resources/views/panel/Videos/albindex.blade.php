8b51a134a0a036b6940f9fff705ae4ad1e521e91
semihycl2397 <smhycl2341@gmail.com>
2021-09-08 13:56:14 +0300

    view has been edited

--- resources/views/panel/Videos/albindex.blade.php
+++ resources/views/panel/Videos/albindex.blade.php
@@ -1,2 +1,27 @@
 @extends('layout.app')
 @section('content')
+
+
+    <div class="container mt-5">
+        <table class="table table-bordered yajra-datatable">
+            <thead>
+            <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Username</th>
+                <th>Phone</th>
+                <th>DOB</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
+    <!--Footer-part-->
+    <div class="row-fluid">
+        <div id="footer" class="span12"> 2021 &copy; Semih Yücel. </a> </div>
+    </div>
+    <!--end-Footer-part-->
+@endsection
